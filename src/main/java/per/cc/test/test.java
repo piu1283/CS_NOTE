ba29dd8141653b15ac21e16266ee36c9091f90df
evan_chan <cc126903@gmail.com>
2020-02-06 19:54:55 -0500

    add some code pices

--- src/main/java/per/cc/test/test.java
+++ src/main/java/per/cc/test/test.java
@@ -2,8 +2,8 @@ package per.cc.test;
 
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by Chen on 1/20/20.
@@ -12,7 +12,7 @@ public class test {
     public static void main(String[] args) {
         // find the date of the second Tuesday on Oct. in this year
         Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR,2020);
+        cal.set(Calendar.YEAR, 2020);
         cal.set(Calendar.MONTH, 9);
         cal.set(Calendar.DAY_OF_WEEK, 3);
         cal.set(Calendar.DAY_OF_WEEK_IN_MONTH, 2);
